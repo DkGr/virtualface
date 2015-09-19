57b684bd03150fb8ea443e122728cb88e4d4114e
Padman <padman@octeau.fr>
2015-09-19 21:49:57 +0200

    Fix commit

--- fb-api.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/vendor/autoload.php';
-
-$fb = new Facebook\Facebook([
-  'app_id' => '117561025264451',
-  'app_secret' => '74dd207dce7d7a9c0be6f8eeba3312b9',
-  'default_graph_version' => 'v2.4',
-  ]);
-?>
